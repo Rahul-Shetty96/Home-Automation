c57d5bef991c555d0c5e5450b7a4110d52bed146
Rahul Shetty <rahul.shetty@ves.ac.in>
2017-03-16 12:46:16 +0530

    Single ONOFF function

---
+++ script_updated.py
@@ -0,0 +1,32 @@
+# Imports
+import webiopi
+import time
+
+ pin = {r1_l:15, r2_f:24, r3_1l:16, r3_2l:21, r4_l:3, r4_t:17 ,r5_l:10, r5_f:11, p:0, l:0, d:0};
+
+# Enable debug output
+webiopi.setDebug()
+
+# Retrieve GPIO lib
+GPIO = webiopi.GPIO
+
+def setup():
+	GPIO.setFunction(15,GPIO.OUT);
+    GPIO.setFunction(24,GPIO.OUT);
+    GPIO.setFunction(16,GPIO.OUT);
+    GPIO.setFunction(21,GPIO.OUT);
+    GPIO.setFunction(3,GPIO.OUT);
+    GPIO.setFunction(17,GPIO.IN);
+    GPIO.setFunction(10,GPIO.OUT);
+    GPIO.setFunction(11,GPIO.OUT);
+    #GPIO.setFunction(3,GPIO.OUT);
+    #GPIO.setFunction(17,GPIO.IN);
+    #GPIO.setFunction(10,GPIO.OUT);
+
+@webiopi.macro
+def ONOFF(id,stat):
+	if(stat == 0)
+	GPIO.digitalWrite(pin[id],1);
+	else
+	GPIO.digitalWrite(pin[id],0);
+	
